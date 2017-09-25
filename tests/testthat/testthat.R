45dc67be81caf78cc1f377d9b007140e855a4a9a
Joao Cunha <joao.cunha@ITs-MacBook-Air.local>
2017-09-25 17:44:21 +0100

    debugging12

--- tests/testthat/testthat.R
+++ tests/testthat/testthat.R
@@ -1,17 +1,20 @@
 
 library(testthat)
-
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(graphics)
+library(maps)
+library(readr)
 
 #Tests for fars_summarize_years() function:
 test_that("Testing fars_summarize_years()", {
-
-  library(magrittr)
 
   years = c("2014","2015")
   summariseDF <- AccidentR::fars_summarize_years(years)
 
   expect_that(summariseDF, is_a("data.frame"))
-  expect_that(ncol(summariseDF), equals(length(years)+1))
+  expect_that(ncol(summariseDF), equals((length(years)+1)))
   expect_that(nrow(summariseDF), equals(12))
 })
 
